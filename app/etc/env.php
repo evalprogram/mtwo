2c8eef672f7df8bc82e20452dcccb193e6a44b93
root <root@magentoinstance.c.dogwood-harmony-173518.internal>
2017-07-24 19:16:15 +0000

    initial commit with default settings

---
+++ app/etc/env.php
@@ -0,0 +1,60 @@
+<?php
+return array (
+  'backend' => 
+  array (
+    'frontName' => 'admin',
+  ),
+  'crypt' => 
+  array (
+    'key' => '1581ffa5c4de78b82882684ecf51f420',
+  ),
+  'session' => 
+  array (
+    'save' => 'files',
+  ),
+  'db' => 
+  array (
+    'table_prefix' => '',
+    'connection' => 
+    array (
+      'default' => 
+      array (
+        'host' => 'localhost',
+        'dbname' => 'magento_two',
+        'username' => 'root',
+        'password' => '@Fingertips33',
+        'active' => '1',
+      ),
+    ),
+  ),
+  'resource' => 
+  array (
+    'default_setup' => 
+    array (
+      'connection' => 'default',
+    ),
+  ),
+  'x-frame-options' => 'SAMEORIGIN',
+  'MAGE_MODE' => 'default',
+  'cache_types' => 
+  array (
+    'config' => 1,
+    'layout' => 1,
+    'block_html' => 1,
+    'collections' => 1,
+    'reflection' => 1,
+    'db_ddl' => 1,
+    'eav' => 1,
+    'customer_notification' => 1,
+    'full_page' => 1,
+    'config_integration' => 1,
+    'config_integration_api' => 1,
+    'translate' => 1,
+    'config_webservice' => 1,
+    'compiled_config' => 1,
+  ),
+  'install' => 
+  array (
+    'date' => 'Mon, 24 Jul 2017 16:57:09 +0000',
+  ),
+);
